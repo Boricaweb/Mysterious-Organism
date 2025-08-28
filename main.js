e003080d8760eec5dab946ab9de2ee37229a3f6b
Boricaweb <thanapolsrangkaew@gmail.com>
2025-08-28 14:34:24 +0700

    3th commit finished requirement 4-5

--- main.js
+++ main.js
@@ -18,11 +18,31 @@ const pAequorFactory = (specimenNum, dna) => {
   return {
     _specimenNum: specimenNum,
     _dna: dna,
+    mutate() {
+      let i = 0;
+      while (i < this._dna.length) {
+        if (returnRandBase() !== this._dna[i]) {
+          this._dna[i] = returnRandBase();
+        }
+        i++;
+      }
+      return this._dna;
+    },
+    compareDNA(pAequor) {
+      let count = 0;
+      for (let i = 0; i < pAequor['dna'].length; i++) {        
+        if (pAequor['dna'][i] === this._dna[i]) {
+          count++;
+        }
+      }
+      let percentage = (count / pAequor['dna'].length) * 100;
+      return `${this._specimenNum} and ${pAequor['specimenNum']} have ${percentage}% DNA in common.`;
+    }
   }
-}
+};
 
-
-
+//console.log(pAequorFactory(specimenNum, mockUpStrand()))
+//console.log(mockUpStrand());
 
 
 
